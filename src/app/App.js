56b8ffd2b2cd09fb325a34bf46808fb2e1fa1af2
cedricngoune <gabyngoune@yahoo.fr>
2021-09-26 21:17:42 +0200

    Project initialization

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from '../pages/Header/header';
+import "antd/dist/antd.css";
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+    </div>
+  );
+}
+
+export default App;
